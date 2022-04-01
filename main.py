62642978e63a43d273e276e5592e9c2a238eb413
Rebecca Kurup Buchholz <rebecca.kurup@wsl.ch>
2022-04-01 15:29:39 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,316 @@
+# TODO work in progress, needs further development
+#
+# Purpose: Read, decode, and clean ARGOS satellite raw data for automated weather stations in Antarctica.
+# Output: Writes NEAD files with the decoded data.
+# TODO add more information about the NEAD format
+#
+# Contributing Authors : Rebecca Buchholz, V.Trotsiuk and Lucia de Espona, Swiss Federal Research Institute WSL
+# Date Last Modified: April 1, 2022
+#
+# TODO clarify example commands
+#
+# Example commands to run main() (make sure virtual environment is activated):
+#   python
+#   from main import main
+#
+# Then call main and pass arguments as needed.
+#
+# No arguments:
+#   main.main()
+#
+# repeatInterval:
+#   main.main(['-r 10'])
+#
+# repeatInterval and localInput:
+#   main.main(['-r 10', '-l True'])
+#
+
+
+import time
+import argparse
+from pathlib import Path
+import configparser
+from datetime import datetime
+from urllib import request
+
+from process_argos import read_argos, decode_argos
+
+# import multiprocessing
+
+# import subprocess
+
+# from gcnet.management.commands.importers.processor.cleaner import CleanerFactory
+# from gcnet.management.commands.importers.processor.process_argos import read_argos, decode_argos
+# from gcnet.management.commands.importers.processor.process_goes import decode_goes
+# from gcnet.util.writer import Writer
+
+import logging
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+def get_parser():
+    parser = argparse.ArgumentParser("ArgosProcessing")
+    parser.add_argument('--repeatInterval', '-r', help='Run continuously every <interval> minutes')
+    parser.add_argument('--localInput', '-l', help='Any string used in this argument will load local input files '
+                                                   'designated in config and skip downloading files from web')
+    return parser
+
+
+def read_config(config_path: str):
+
+    config_file = Path(config_path)
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    logger.info(f' Read configuration file: {config_path}')
+
+    if len(config.sections()) < 1:
+        logger.error(' Invalid config file, missing sections')
+        raise ValueError('Invalid config file, missing sections')
+
+    return config
+
+
+def get_writer_config_dict(config_parser: configparser):
+    config_dict = dict(config_parser.items('file'))
+    config_dict['columns'] = dict(config_parser.items('columns'))
+    return config_dict
+
+
+# Import dependencies
+def get_input_data(config_dict: dict, local_input):
+
+    # If command line localInput argument passed (with any string) assign data_file to 'data_local' from config
+    if local_input:
+        data_file = config_dict['data_local']
+        logger.info(f' Skipping downloading input data, using local file: {data_file}')
+
+    # Else assign data_file to file downloaded from data_url
+    else:
+        data_url = config_dict['data_url']
+        data_file = config_dict['data_url_file']
+
+        # Download data from URL
+        with request.urlopen(data_url) as data:
+            content = data.read()
+
+        # Save data to data_file
+        with open(data_file, 'wb') as download_file:
+            download_file.write(content)
+
+        logger.info(f' Downloaded input data from URL and wrote file: {data_file}')
+
+    return data_file
+
+
+# def get_csv_import_command_list(config_parser: configparser, station_type: str):
+#
+#     # Load stations configuration file and assign it to stations_config
+#     stations_config = config_parser
+#
+#     # Assign variable to contain command list
+#     commands = []
+#
+#     # Assign variables to stations_config values and loop through each station in stations_config, create list of
+#     # command strings to run csv imports for each station
+#     for section in stations_config.sections():
+#
+#         # Check config key 'active' if data for station should be processed
+#         # A value of 'True' means that station data will be processed
+#         # Any other value means that station data will not be processed
+#         if stations_config.get(section, 'active') == 'True' and stations_config.get(section, 'type') == station_type:
+#
+#             csv_input = stations_config.get(section, 'csv_input')
+#             model = stations_config.get(section, 'model')
+#             csv_data = stations_config.get(section, 'csv_data_dir')
+#
+#             # Previous version used 'import_data' management command to import data
+#             # csv_temporary = stations_config.get(section, 'csv_temporary')
+#             # command_string = f'python manage.py import_data -s {csv_temporary} -c gcnet/config/stations.ini ' \
+#             #                  f'-i {csv_data}/{csv_input} -m {model} -f True'
+#
+#             # Management command 'import_csv' will be used to import processed station data stored locally
+#             # into corresponding database model
+#             command_string = f'python manage.py import_csv -s local -i {csv_data}/{csv_input} -a gcnet -m {model}'
+#
+#             commands.append(command_string)
+#
+#     return commands
+
+
+# def execute_commands(commands_list):
+#     # Iterate through commands_list and execute each command
+#     for station_command in commands_list:
+#         try:
+#             subprocess.run(station_command, shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
+#         except subprocess.CalledProcessError:
+#             logger.error(f'Could not run command: {station_command}')
+#             continue
+
+
+# TODO import functions and any needed dependencies
+def process_argos_data(config_dict: dict, local_input=None):
+
+    # Get input data
+    data = get_input_data(config_dict, local_input)
+
+    # Get writer configured for the cleaner output
+    # writer = Writer.new_from_dict(config_dict['writer'])
+
+    data_raw = read_argos(data, nrows=None)
+    data_decode = decode_argos(data_raw, remove_duplicate=True, sort=True)
+
+    # Decode ARGOS data
+    # if station_type == 'argos':
+    #     data_raw = read_argos(data, nrows=None)
+    #     data_decode = decode_argos(data_raw, remove_duplicate=True, sort=True)
+    #
+    # # Decode GOES data
+    # elif station_type == 'goes':
+    #     data_decode = decode_goes(data)
+    #
+    # else:
+    #     logger.error(f' Invalid station type: {station_type}')
+    #     raise ValueError(f'Invalid station type: {station_type}')
+
+    # Convert decoded data pandas dataframe to Numpy array
+    data_array = data_decode.to_numpy()
+
+    # Clean data and write csv and json files
+    # stations_config_path = 'gcnet/config/stations.ini'
+    # cleaner = CleanerFactory.get_cleaner(station_type, stations_config_path, writer)
+    #
+    # if not cleaner:
+    #     logger.error(f'No cleaner exists for station type: {station_type}')
+    #     raise ValueError(f'No cleaner exists for station type: {station_type}')
+
+    # Clean Numpy array data by applying basic filters
+    # Cleaner also writes csv and json files
+    # cleaner.clean(data_array)
+
+    return
+
+
+def main(args=None):
+    """
+    Main entry point for processing ARGOS satellite transmissions.
+    """
+
+    # Access arguments passed in command line
+    parser = get_parser()
+    args = parser.parse_args(args)
+
+    # Read config file
+    config_path = 'config/config.ini'
+    config = read_config(config_path)
+
+    if not config:
+        logger.error(f'Not valid config file: {config_path}')
+        return -1
+
+    # Process and clean input data, write csv and json files, import csv files data into Postgres database
+    repeat = True
+    while repeat:
+
+        # Do not repeat if the -r argument is not present
+        repeat = (args.repeatInterval is not None)
+
+        start_time = time.time()
+
+        logger.info(" **************************** START DATA PROCESSING ITERATION (start time: {0}) "
+                    "**************************** "
+                    .format(datetime.fromtimestamp(start_time)
+                            .strftime('%Y-%m-%d %H:%M:%S')))
+
+        # Get config_dict configured for 'argos'
+        config_dict = dict(config.items('argos'))
+        config_dict['writer'] = get_writer_config_dict(config)
+
+        local_input = None
+        # Assign local_input if commandline option localInput is passed
+        if args.localInput:
+            local_input = args.localInput
+
+        # Process ARGOS data
+        process_argos_data(config_dict, local_input)
+
+        # # Assign empty processes list
+        # processes = []
+        #
+        # # Start process
+        # for station_type in ['argos', 'goes']:
+        #
+        #     # Get config_dict configured for station_type
+        #     config_dict = dict(config.items(station_type))
+        #     config_dict['writer'] = get_writer_config_dict(config)
+        #
+        #     local_input = None
+        #     # Assign local_input if commandline option localInput is passed
+        #     if args.localInput:
+        #         local_input = args.localInput
+        #
+        #     # Process data from each station_type concurrently using multiprocessing
+        #     process = multiprocessing.Process(target=process_data, args=(station_type, config_dict, local_input))
+        #     processes.append(process)
+        #     process.start()
+        #
+        # for process in processes:
+        #     process.join()
+
+        # TODO output data in NEAD format, one NEAD file per station
+
+        # # Write short-term csv files
+        # station_array = list((config.get("file", "stations")).split(","))
+        # csv_short_days = int(config.get("file", "short_term_days"))
+        # csv_writer_config = get_writer_config_dict(config)
+        # csv_writer = Writer.new_from_dict(csv_writer_config)
+        # csv_writer.write_csv_short_term(station_array, csv_short_days)
+
+        # # Read the stations config file
+        # stations_path = 'gcnet/config/stations.ini'
+        # stations_config = read_config(stations_path)
+        #
+        # # Check if stations_config exists
+        # if not stations_config:
+        #     logger.error("Non-valid config file: {0}".format(stations_path))
+        #     return -1
+
+        # Import csv files into Postgres database so that data are available for API
+        # logger.info(" **************************** START DATA IMPORT ITERATION **************************** ")
+        #
+        # # Assign empty import_processes list
+        # import_processes = []
+        #
+        # # Get the import commands
+        # goes_commands = get_csv_import_command_list(stations_config, 'goes')
+        # argos_commands = get_csv_import_command_list(stations_config, 'argos')
+        #
+        # # Create list with both ARGOS and GOES commands
+        # import_commands = [goes_commands, argos_commands]
+        #
+        # # Process ARGOS and GOES import commands in parallel
+        # for command_list in import_commands:
+        #     process = multiprocessing.Process(target=execute_commands, args=(command_list,))
+        #     import_processes.append(process)
+        #     process.start()
+        #
+        # for process in import_processes:
+        #     process.join()
+
+        exec_time = int(time.time() - start_time)
+        logger.info(f' FINISHED data processing iteration, that took {exec_time} seconds')
+
+        if repeat:
+            interval = int(args.repeatInterval) * 60
+            if interval > exec_time:
+                wait_time = interval - exec_time
+                logger.info(f' SLEEPING {wait_time} seconds before next iteration...\n')
+                time.sleep(wait_time)
+
+    return 0
+
+
+if __name__ == '__main__':
+    main()
---
+++ process_argos.py
@@ -0,0 +1,219 @@
+#
+# ARGOS satellite data processing functions
+
+import pandas
+import numpy
+
+import logging
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+def read_argos(file, nrows):
+    """
+    Read the Argos raw file with Pandas.
+    Reshape it to the one row per transmission (from 4 columns) and return a pandas df.
+    :param file: path to the Argos raw file
+    :param nrows: number of rows to be read, default is 300000
+    :return: a pandas dataframe, with 24 columns
+    """
+
+    logger.info(f' Reading and processing {file}...')
+
+    # Set up the column width, column names and the timestamp split
+    columns = [(0, 6), (6, 28), (28, 39), (39, 52), (52, 65), (65, -1)]
+    columns_names = ['Station', 'Timestamp', 'Column1', 'Column2', 'Column3', 'Column4']
+    columns_timestamp = ['Year', 'Month', 'Day', 'Hours', 'Minutes', 'Seconds', 'Substation']
+
+    # Assign skip_rows to 0-indexed list of line numbers that should skipped and not read into the data frame
+    skip_rows = get_search_strings_line_numbers(
+        file,
+        ['/Invalid day of the month: {0}: begin date is posterior to the last day of the year', 'ARGOS READY']
+    )
+
+    # Read the raw file with predefined columns
+    df = pandas.read_fwf(file, names=columns_names, colspecs=columns, nrows=nrows, skiprows=skip_rows,
+                         converters={'Station': str, 'Timestamp': str,
+                                     'Column1': int, 'Column2': int, 'Column3': int, 'Column4': int})
+
+    # Copy the station down so the part one and part two have them
+    # Remove the rows where with the satelite information as it doesn't care any additional info
+    df.loc[df['Station'].notnull(), 'Station'] = df.loc[df['Station'].notnull(), 'Timestamp'].str[0:6]
+    df['Station'] = df['Station'].ffill()
+    df = df.dropna(subset=columns_names[2:], how='all', inplace=False)
+
+    # For each timestamp trunsmission create a separate group which later
+    # will be used to set the index to transform to wide format
+    df['timestamp_group'] = 0
+    df.loc[~df['Timestamp'].isnull(), 'timestamp_group'] = 1
+    df['timestamp_group'] = df['timestamp_group'].cumsum()
+
+    # Fill down the information about the timestamp
+    df['Timestamp'] = df['Timestamp'].ffill()
+
+    # Count the row for each group for the transformation
+    df["timestamp_row"] = df.groupby('timestamp_group').cumcount()
+
+    # Transform data.frame to the wide format
+    df = df.set_index(columns_names[:2] + ['timestamp_group'] + ['timestamp_row']).unstack()
+
+    # Set new names and drop index
+    df.columns = [f'v_{i}' for i in range(1, 17)]
+    df = df.reset_index()
+
+    # Rearange the data to correct order (same as fortran output) and set the names again
+    df[columns_timestamp] = df['Timestamp'].str.split('-|:| ', 6, expand=True)
+    df = df[columns_timestamp + ['Station', 'v_1', 'v_5', 'v_9', 'v_13', 'v_2', 'v_6', 'v_10',
+                                 'v_14', 'v_3', 'v_7', 'v_11', 'v_15', 'v_4', 'v_8', 'v_12', 'v_16']]
+    df.columns = columns_timestamp + ['Station'] + [f'v_{i}' for i in range(1, 17)]
+
+    return df
+
+
+def decode_argos(df, remove_duplicate=True, sort=True):
+    """
+    Decode the output of the `read_argos` from bits to the  numbers
+    :param df: a pandas dataframe as an output of `read_argos` function
+    :param remove_duplicate: whether to remove duplicated rows (Can be removed later)
+    :param sort: whether to sort the output (Can be sorted later)
+    :return: a pandas dataframe
+    """
+
+    logger.info(f' Decoding data...')
+
+    # Drop duplicated rows, this substantially speeds up the process
+    if remove_duplicate:
+        df = df.drop_duplicates(subset=['Station'] + [f'v_{i}' for i in range(1, 17)], inplace=False)
+
+    # Convert to the numpy array
+    df = df.to_numpy(dtype='float', na_value=numpy.nan)
+
+    # Correct year
+    df[:, 9] = correct_year(df[:, 0], df[:, 9])
+
+    # Vectorise the argos function and apply it to all columns
+    f_argos_bit_v = numpy.vectorize(f_argos_bit)
+    df[:, 8:24] = f_argos_bit_v(df[:, 8:24])
+
+    # Put it back to the pandas dataframe and sort
+    df = pandas.DataFrame(df)
+    df.columns = ['Year', 'Month', 'Day', 'Hours', 'Minutes', 'Seconds', 'Substation', 'Station'] + [f'v_{i}' for i in
+                                                                                                     range(1, 17)]
+
+    # Convert Logger ID (v_1) into the integer. The fortran code truncated the values.
+    df['v_1'] = df['v_1'].astype('int')
+
+    # This shall represent the FORTRAN formating
+    # df['v_1'] = df['v_1'].apply("{:8.0f}".format)
+    # df['v_2'] = df['v_2'].apply("{:8.0f}".format)
+    #
+    # df['v_1'] = df['v_1'].astype('int')
+    # df['v_2'] = df['v_2'].astype('int')
+
+    # Again remove duplicates. It can hapend that some still remained
+    if remove_duplicate:
+        df = df.drop_duplicates(subset=['Station'] + [f'v_{i}' for i in range(1, 17)], inplace=False)
+
+    if sort:
+        # df = df.sort_values(by=['Station', 'v_1', 'Year', 'Month', 'Day', 'Hours', 'Minutes', 'Seconds'],
+        #                     ascending=True)
+        # TODO figure out best way to sort values for testing while comparing to Fortran .dat output
+        # TEST
+        df = df.sort_values(by=['Year', 'Month', 'Day', 'Station', 'Hours', 'Minutes', 'Seconds'],
+                            ascending=True)
+
+    return df
+
+
+def correct_year(x, y):
+    """
+    Providing two arrays check where the year is not equal to the satelite year + 1
+    :param x: vector of years from satelite transmission
+    :param y: vector of years from the data
+    :return: corrected vector of years
+    """
+    # Make sure the year is the same derived from satellite and data logger
+    ind = x == y + 1
+    y[ind] = x[ind]
+
+    return y
+
+
+def f_argos_bit(x):
+    """
+    Support function to decode each single binary variable to the standard output.
+    :param x: a value to be decoded
+    :return: a single real value
+    """
+
+    if x is None:
+        out = None
+    # Test if the value is not empty
+    # Carefull python has indexing 0 comparte to R or Fortran where it is 1
+    else:
+        # Make a binary decoding
+        # Adding an empty vector for the output
+        out = 0
+        ins = [0] * 16
+
+        # Loop through each of 16 numbers and gradual decrease the value of x
+        for k in range(1, 17):
+
+            if x >= 2 ** (16 - k):
+
+                x = x - 2 ** (16 - k)
+                ins[k - 1] = 1
+
+                if k >= 4:
+                    out = out + 2 ** (16 - k)
+
+        if ins[0] == 1:
+            out = out * -1
+        # if(ins[2] == 0 & ins[3] == 0) out = out
+        if ins[1] == 0 and ins[2] == 1:
+            out = out / 10
+        if ins[1] == 1 and ins[2] == 0:
+            out = out / 100
+        if ins[1] == 1 and ins[2] == 1:
+            out = out / 1000
+
+    return out
+
+
+def write_csv(df, file):
+    """
+    Write a pandas dataframe to the csv file
+    :param df: a pandas data frame
+    :param file: a location of the file
+    :return: nothing
+    """
+    df.to_csv(file)
+
+
+def get_search_strings_line_numbers(input_file, strings_to_search):
+    """
+        ASSUMPTION: Returned list of line numbers that are 0-indexed!!!!!!
+        :param input_file: path to the input file (raw ARGOS satellite data)
+        :param strings_to_search: list of search strings, if a row has one of these search strings then it will be
+            excluded in the Argos processing
+        :returns Searches for the strings_to_search in input file and
+            returns 0-indexed list of line numbers containing those strings
+    """
+
+    line_number = -1
+    line_number_list = []
+
+    with open(input_file, 'r') as r:
+
+        for line in r:
+
+            line_number += 1
+
+            for item in strings_to_search:
+
+                if item in line:
+                    line_number_list.append(line_number)
+
+    return line_number_list
